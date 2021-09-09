68c362a0a643fa207ee5055329be10d8b07b0034
Eben2048009 <89779194+Eben2048009@users.noreply.github.com>
2021-09-09 18:18:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__) 
+app.config['SECRET_KEY'] = '8031b396580be80eed28e7178f0b40eb'
+
+
+posts = [
+    {
+        'author': 'Eben Emmanuel',
+        'title': 'Flask 101: An Introduction',
+        'content': 'Getting Started with Flask',
+        'date_posted': 'July 11 2021'
+    },
+    {
+        'author': 'Mia Baslin Jolly',
+        'title': 'Swimming and Karate',
+        'content': 'Balancing all the tedious tasks each day',
+        'date_posted': 'July 7 2021'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts= posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title= 'About')
+
+@app.route('/register', methods= ['GET', 'POST'])
+def register():
+    form= RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')      ## Creating a Flash Message
+        return redirect(url_for('home'))                                    ## If success then Redirect to home page
+    return render_template('register.html', title= 'Register', form= form)
+
+@app.route('/login')
+def login():
+    form= LoginForm()
+    return render_template('login.html', title= 'Login', form= form)
+
+if __name__ == '__main__':
+    app.run(host= '127.0.0.1', port= 5000, debug = True)  
